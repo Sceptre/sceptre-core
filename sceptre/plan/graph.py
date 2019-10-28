76fb98003d38c29383251ba4be68fe32b1ff9d45
Niall Grant <ngfgrant@gmail.com>
2019-10-28 16:08:59 +0000

    Add StackGraph to Core

--- sceptre/plan/graph.py
+++ sceptre/plan/graph.py
@@ -27,17 +27,24 @@ class StackGraph:
         :type stacks: set
         """
         self.logger = logging.getLogger(__name__)
-        self.graph = nx.DiGraph()
+        self.__graph = nx.DiGraph()
         self._generate_graph(stacks)
 
+    @property
+    def graph(self):
+        return self.__graph
+
     def __repr__(self):
-        return str(nx.convert.to_dict_of_lists(self.graph))
+        return str(nx.convert.to_dict_of_lists(self.__graph))
 
     def __iter__(self):
-        return self.graph.__iter__()
+        return self.__graph.__iter__()
+
+    def __len__(self):
+        return self.__graph.number_of_nodes()
 
     def filtered(self, source_stacks, reverse=False):
-        graph = (nx.reverse if reverse else nx.DiGraph)(self.graph)
+        graph = (nx.reverse if reverse else nx.DiGraph)(self.__graph)
 
         relevant = set(source_stacks)
         for stack in source_stacks:
@@ -55,7 +62,7 @@ class StackGraph:
         StackGraph. The number of incoming edge also represents the number
         of Stacks that depend on the given Stack.
         """
-        return self.graph.in_degree(stack)
+        return self.__graph.in_degree(stack)
 
     def remove_stack(self, stack):
         """
@@ -63,42 +70,40 @@ class StackGraph:
         all adjecent edges that represent a 'depends on' relationship with
         other Stacks.
         """
-        return self.graph.remove_node(stack)
+        return self.__graph.remove_node(stack)
 
     def _generate_graph(self, stacks):
         """
-        Generates the graph for the StackGraph object.
+        Generates the StackGraph.
 
         :param stacks: A set of Stacks
         :type stacks: set
         """
 
-        for stack in stacks:
-            self._generate_edges(stack, stack.config.dependencies)
-        self.graph.remove_edges_from(nx.selfloop_edges(self.graph))
+        self.__graph.add_nodes_from(stacks)
+        self._generate_edges()
 
-    def _generate_edges(self, stack, dependencies):
-        """
-        Adds edges to the graph based on a list of dependencies that are
-        generated from the inital stack config. Each of the paths
-        in the inital_dependency_paths list are a depency that the inital
-        Stack config depends on.
+    def as_pydot(self):
+        return nx.drawing.nx_pydot.to_pydot(self.__graph)
 
-        :param stack: A Sceptre Stack
-        :type stack: sceptre.provider.stack.Stack
-        :param dependencies: a collection of dependency paths
-        :type dependencies: list
-        """
-        self.logger.debug(
-            "Generate dependencies for stack {0}".format(stack)
-        )
-        for dependency in dependencies:
-            self.graph.add_edge(dependency, stack)
-            if not nx.is_directed_acyclic_graph(self.graph):
-                raise CircularDependenciesError(
-                    "Dependency cycle detected: {} {}".format(stack,
-                                                              dependency))
-            self.logger.debug("  Added dependency: {}".format(dependency))
+    def _generate_edges(self):
+        for stack in self.__graph.nodes():
+            self.logger.debug(
+                "Generate dependencies for stack {0}".format(stack)
+            )
 
-        if not dependencies:
-            self.graph.add_node(stack)
+            if stack.config.dependencies:
+                dep = stack.config.dependencies
+                dep_node = self.__get_stack_from_graph_by_id(dep)
+                self.__graph.add_edge(dep_node, stack)
+                if not nx.is_directed_acyclic_graph(self.__graph):
+                    raise CircularDependenciesError(
+                        "Dependency cycle detected: {} {}".format(stack, dep))
+                self.logger.debug("  Added dependency: {}".format(dep))
+
+        self.__graph.remove_edges_from(nx.selfloop_edges(self.__graph))
+
+    def __get_stack_from_graph_by_id(self, stack_id):
+        for node in self.__graph.copy():
+            if node.id == stack_id:
+                return node
--- tests/test_core.py
+++ tests/test_core.py
@@ -8,18 +8,15 @@ from sceptre.provider.stack import Stack
 def stack_map():
     stack_map = {
         "/var/data/config/stack.yaml": {
-            'itemA': 'value_a',
-            'stack_config': {
-                "region": "eu-west-1",
-                'template_path': 'templates/vpc.json',
-            }
+            "name": "Stack1",
+            "region": "eu-west-1",
+            'template_path': 'templates/vpc.json',
+            'dependencies': '/var/data/config/stack_b.yaml'
         },
         "/var/data/config/stack_b.yaml": {
-            'itemA': 'value_a',
-            'stack_config': {
-                "region": "eu-west-2",
-                'template_path': 'templates/vpc.json',
-            }
+            "name": "Stack2",
+            "region": "eu-west-2",
+            'template_path': 'templates/vpc.json',
         },
     }
     return stack_map
@@ -32,4 +29,14 @@ class TestCore:
 
     def test_core_stack_list_contains_stacks(self, stack_map):
         core = SceptreCore(stack_map)
-        assert isinstance(core.stacks[0], Stack)
+        assert isinstance(core.stacks.pop(), Stack)
+
+    def test_stacks_are_added_to_graph(self, stack_map):
+        core = SceptreCore(stack_map)
+        assert len(core.graph) == len(stack_map)
+
+    def test_stack_dependencies_are_correct(self, stack_map):
+        core = SceptreCore(stack_map)
+        for a, b in core.graph.graph.edges:
+            assert a.id == '/var/data/config/stack_b.yaml' \
+                and b.id == '/var/data/config/stack.yaml'
