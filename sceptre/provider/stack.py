b8a6ed52ded2297c57f0bff7049d838e36edb70c
Niall Grant <ngfgrant@gmail.com>
2019-10-28 16:07:17 +0000

    Change Stack.id from hash of value to value
    - also loosen constraint of AttributeError on getting non-existent
    attributes on StackConfigData, instead return None.

--- sceptre/provider/stack.py
+++ sceptre/provider/stack.py
@@ -7,7 +7,6 @@ This module implements a Stack class, which stores a Stack's data.
 """
 
 import logging
-import hashlib
 
 
 class Stack:
@@ -20,7 +19,7 @@ class Stack:
         if not isinstance(config, StackConfigData):
             raise TypeError("Config must be of type StackConfigData")
         self.config = config
-        self.id = self.__hash_id(stack_id)
+        self.id = stack_id
 
     def __repr__(self):
         return(
@@ -34,27 +33,22 @@ class Stack:
         )
 
     def __str__(self):
-        return self.config.name
+        return self.id
 
     def __eq__(self, stack):
         return(
-            self.id == stack.id and
-            self.config == stack.config
+            self.id == stack.id
         )
 
     def __hash__(self):
         return hash(str(self))
-
-    def __hash_id(self, stack_id):
-        return hashlib.sha256(stack_id.encode('utf-8')).hexdigest()
 
 
 class StackConfigData(dict):
     def __getattr__(self, attr):
         if attr in self:
             return self[attr]
-        else:
-            raise AttributeError("No attribute {} on StackConfigData".format(attr))
+        return None
 
     def __setattr__(self, attr, value):
         self[attr] = value
